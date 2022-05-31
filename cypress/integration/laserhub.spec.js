5556654bb8cc6048b59872c41c91fe5db04828c6
CMontanha <carlosmontanha@gmail.com>
2022-05-31 23:35:45 +0100

    master

---
+++ cypress/integration/laserhub.spec.js
@@ -0,0 +1,14 @@
+import login from '../fixtures/login.json';
+
+describe('Laserhub login test', () => {
+
+  before(() => {
+    cy.visit('https://app.laserhub.com/login')
+  })
+
+  it('Login', () => {
+
+    //Accept cookies button
+
+  })
+})
