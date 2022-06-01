e062f1ece6f8ee993b268d1373273b05da35ea11
CMontanha <carlosmontanha@gmail.com>
2022-06-01 16:13:50 +0100

    master

--- cypress/integration/laserhub.spec.js
+++ cypress/integration/laserhub.spec.js
@@ -4,11 +4,21 @@ describe('Laserhub login test', () => {
 
   before(() => {
     cy.visit('https://app.laserhub.com/login')
+    cy.fixture('login').as('username')
   })
 
   it('Login', () => {
 
     //Accept cookies button
+    cy.get('#onetrust-accept-btn-handler')
+      .should('be.visible')
+      .click()
 
+    //Insert login information
+    cy.get('#email').should('be.visible')
+      .type('kkkkkk', {force:true})
+    cy.get('#password')
+      .type('yyyyyyy', {force:true})
+    cy.get('button').contains('Login').click()
   })
 })
