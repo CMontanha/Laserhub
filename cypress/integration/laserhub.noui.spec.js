a8fd9f1bbc43d3fab0cf0062c46f98ac9c7e2754
CMontanha <35295764+CMontanha@users.noreply.github.com>
2022-07-04 14:56:29 +0100

    master

--- cypress/integration/laserhub.noui.spec.js
+++ cypress/integration/laserhub.noui.spec.js
@@ -3,18 +3,51 @@ import login from '../fixtures/login.js';
 Cypress.Cookies.debug(true)
 
 beforeEach(() => {
+  cy.setCookies()
   cy.visit('https://app.laserhub.com/')
 })
 
-describe('Cookie Test', () => {
+describe('Laserhub login test', () => {
 
-  it.only('Login with cookie', () => {
-    
-    //Accept cookies with a request
-    cy.acceptCookies()
+  it.only('Login', () => {
 
-    cy.loginRequest()
+    cy.loginRequest().then((response) => {
+      cy.visit('https://app.laserhub.com' + response.body.redirectURL)
+    })
 
+    //Confirming we have a loged in session inspecting cookies
+    cy.getCookie('session_laserhub')
+      .should('not.be.empty')
+
+  })
+})
+
+describe('Cannot login with wrong information', () => {
+
+  it('Wrong username, correct password', () => {
+
+  })
+
+  it('Correct username, wrong password', () => {
+
+  })
+
+  it('Both empty fields', () => {
+
+  })
+
+  it('User empty field', () => {
+
+  })
+
+  it('Password empty field', () => {
+
+  })
+})
+
+describe('Logout works', () => {
+
+  it('Login and logout', () => {
     
   })
 })
